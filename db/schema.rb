2bcea5398321cb7b569feb52136fb788449c0ca4
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-05-30 05:42:44 +0200

    Added association

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140530031124) do
+ActiveRecord::Schema.define(version: 20140530031834) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
-    t.string   "user_id"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
